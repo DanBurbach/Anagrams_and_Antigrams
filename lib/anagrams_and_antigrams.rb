5254a6fceaa6f8071d0d36083dc16644df9c82a0
Daniel Burbach <daniel@>
2018-11-16 11:37:30 -0800

    passing first test failing on identifiying numbers test

--- lib/anagrams_and_antigrams.rb
+++ lib/anagrams_and_antigrams.rb
@@ -2,8 +2,13 @@ class String
   def word_entry
     inputted_words = self
     inputted_words.downcase!()
+    inputted_words.gsub!(/[!@#$%^&*()-=_+|;':",.<>?']/, '')
+    #binding.pry
     inputted_words.gsub!(/\s+/, "")
-    inputted_words.gsub(/[[:punct:]]/, '')
+    #binding.pry
+    #if (self.(/\d/))
+    #  puts "Please no numbers in the words!"
+    #end
     return inputted_words
   end
 end
