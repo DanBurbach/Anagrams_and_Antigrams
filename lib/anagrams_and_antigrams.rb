55122e11f5343973f2825db2cf49a86448e90a48
Daniel Burbach <daniel@>
2018-11-16 11:21:53 -0800

    added second test to check or numbers failed test

--- lib/anagrams_and_antigrams.rb
+++ lib/anagrams_and_antigrams.rb
@@ -2,9 +2,8 @@ class String
   def word_entry
     inputted_words = self
     inputted_words.downcase!()
-    #binding.pry
     inputted_words.gsub!(/\s+/, "")
-    #binding.pry
+    inputted_words.gsub(/[[:punct:]]/, '')
     return inputted_words
   end
 end
--- spec/anagrams_and_antigrams_spec.rb
+++ spec/anagrams_and_antigrams_spec.rb
@@ -4,8 +4,12 @@ require('pry')
 
 
 describe '#word_entry' do
-  it("turns all letters in a phrase to lower case and removes all spaces") do
-    expect("Clint Eastwood!".word_entry).to eql("clinteastwood!")
+  it("turns all letters in a phrase to lower case and removes all spaces and punctuation") do
+    expect("Clint Eastwood!".word_entry).to eql("clinteastwood")
+  end
+  it("returns a false if any numbers found") do
+    expect("Clint1234 Eastwood!".word_entry).to eql("Please no numbers in the words!")
   end
 end
-#Clint Eastwood = Old West action
+#Clint Eastwood! = Old West action!
+#clinteastwood = oldwestaction
