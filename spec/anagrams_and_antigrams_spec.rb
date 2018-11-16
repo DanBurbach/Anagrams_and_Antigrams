18442a2d9c497849599b3678588d22f9b492971d
Daniel Burbach <daniel@>
2018-11-16 11:02:48 -0800

    combined first and second test now passing both

--- spec/anagrams_and_antigrams_spec.rb
+++ spec/anagrams_and_antigrams_spec.rb
@@ -4,11 +4,8 @@ require('pry')
 
 
 describe '#word_entry' do
-  it("turns all letters in a phrase to lower case") do
-    expect("Clint Eastwood!".word_entry).to eql("clint eastwood!")
-  end
-  it("removes all spaces") do
-    expect("clint eastwood!".word_entry).to eql("clinteastwood!")
+  it("turns all letters in a phrase to lower case and removes all spaces") do
+    expect("Clint Eastwood!".word_entry).to eql("clinteastwood!")
   end
 end
 #Clint Eastwood = Old West action
