e1349572b771f09568a9b0f6ed758534f18e90dc
Daniel Burbach <daniel@>
2018-11-16 14:40:19 -0800

    turns the word into an array of letters

--- lib/anagrams_and_antigrams.rb
+++ lib/anagrams_and_antigrams.rb
@@ -1,14 +1,17 @@
 class String
-  def word_entry
-    if self.gsub(/[\d]/)&&self.gsub(/[a,e,i,o,u,y]/)
-      return "This isn't a valid entry, please enter an actual word or words!!"
+  def anagrams
+    #if self.gsub(/[\d]/)&&self.gsub(/[a,e,i,o,u,y]/)
+    #binding.pry
+    #  return "This isn't a valid entry, please enter an actual word or words!!"
 
-    else
+    #else
       inputted_words = self
       inputted_words.downcase!()
       inputted_words.gsub!(/[!@#$%^&*()-=_+|;':",.<>?']/, '')
       inputted_words.gsub!(/\s+/, "")
+      inputted_words.scan!(/\w/)
+      binding.pry
       return inputted_words
-    end
+    #end
   end
 end
--- spec/anagrams_and_antigrams_spec.rb
+++ spec/anagrams_and_antigrams_spec.rb
@@ -3,13 +3,13 @@ require('anagrams_and_antigrams.rb')
 require('pry')
 
 
-describe '#word_entry' do
-  #it("changes letters in a phrase to lower case and removes all #spaces and punctuation") do
-  #  expect("Clint Eastwood!".word_entry).to eql("clinteastwood")
-  #end
-  it("when any numbers found or no vowels found") do
-    expect("ytrvb123".word_entry).to eql("This isn't a valid entry, please enter an actual word or words!!")
+describe '#anagrams' do
+  it("changes letters in a phrase to lower case and removes all spaces and punctuation and then turns into an array of letterss") do
+    expect("Clint Eastwood!".anagrams).to eql(["c","l","i","n","t","e","a","s","t","w","o","o","d"])
   end
+  #it("when any numbers found or no vowels found") do
+  #  expect("ytrvb123".anagrams).to eql("This isn't a valid entry, please enter an actual word or words!!")
+  #end
 end
 #Clint Eastwood! = Old West action!
 #clinteastwood = oldwestaction
