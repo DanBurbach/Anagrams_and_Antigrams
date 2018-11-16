d14ec424a29bf42a4539fa785a4a39c6bfae2adc
Daniel Burbach <daniel@>
2018-11-16 13:40:02 -0800

    identifies words with numbers and minimizes all other words down to a letter chain

--- lib/anagrams_and_antigrams.rb
+++ lib/anagrams_and_antigrams.rb
@@ -1,18 +1,17 @@
 class String
   def word_entry
-    inputted_words = self
+    if self.gsub(/[^\d]/, '')
+      return "Please no numbers in the words!"
+
+    elsif (self.gsub(/\d/))
+      return "This isn't a valid entry, please enter an actual word or words!!"
+
+    else
+      inputted_words = self
       inputted_words.downcase!()
       inputted_words.gsub!(/[!@#$%^&*()-=_+|;':",.<>?']/, '')
       inputted_words.gsub!(/\s+/, "")
-
       return inputted_words
-    def word_error
-      if self.(/\d/)
-        return "Please no numbers in the words!"
-      end
-      #elsif (self.(/\d/))
-      #  puts "This isn't a valid entry, please enter an #actual word or words!!"
-      #end
     end
   end
 end
--- spec/anagrams_and_antigrams_spec.rb
+++ spec/anagrams_and_antigrams_spec.rb
@@ -4,16 +4,16 @@ require('pry')
 
 
 describe '#word_entry' do
-  it("turns all letters in a phrase to lower case and removes all spaces and punctuation") do
+  it("changes letters in a phrase to lower case and removes all spaces and punctuation") do
     expect("Clint Eastwood!".word_entry).to eql("clinteastwood")
   end
-  it("returns a false if any numbers found") do
-    expect("Clint1234 Eastwood!".word_error).to eql("Please no numbers in the words!")
-  end
-  #it("returns a false if no vowels found") do
-  #  expect("Clnt Estwd!".word_entry).to eql("This #isn't a valid entry, please enter an actual #word or words!")
-  #end
 
+  it("when any numbers found") do
+    expect("Clint1234 Eastwood!".word_entry).to eql("Please no numbers in the words!")
+  end
+  it("returns a false if no vowels found") do
+   expect("Clnt Estwd!".word_entry).to respond_with "This isn't a valid entry, please enter an actual word or words!"
+  end
 end
 #Clint Eastwood! = Old West action!
 #clinteastwood = oldwestaction
