5bd3147463aa26b618b99f74d3e4c0d6c0ca6c05
Danno <41841936+DanBurbach@users.noreply.github.com>
2018-11-19 09:59:44 -0800

    Add files via upload
    Changed tests and working on passing through two entries to pass tests

--- lib/anagrams_and_antigrams.rb
+++ lib/anagrams_and_antigrams.rb
@@ -1,24 +1,26 @@
 class Anagram
-  def user_entries(word1,word2)
+  def user_entries(word1, word2)
     @word1 = word1
     @word2 = word2
   end
-
+  def word1
+    @word1
+  end
+  def word2
+    @word2
+  end
   def anagram_filter
     user_entries = [@word1, @word2]
-    x = @word1.chars.to_a.sort
-    y = @word2.chars.to_a.sort
+    array_word1 = @word1.chars.to_a.sort
+    array_word2 = @word2.chars.to_a.sort
     if user_entries.gsub(/[\d]/)&&user_entries.gsub(/[a,e,i,o,u,y]/)
-    binding.pry
       return "This isn't a valid entry, please enter an actual word or words!!"
     else
       user_entries.downcase!()
       user_entries.gsub!(/[!@#$%^&*()-=_+|;':",.<>?']/, '')
       user_entries.gsub!(/\s+/, "")
-    binding.pry
       split_words = inputted_words.chars.to_a
-
-      if (x == y) == true
+      if (array_word1 == array_word2) == true
         return "These words are anagrams"
       else
         return "These words are antigrams"
--- spec/anagrams_and_antigrams_spec.rb
+++ spec/anagrams_and_antigrams_spec.rb
@@ -4,22 +4,46 @@ require('pry')
 
 
 describe '#anagram_filter' do
-  it("when the words have the same number of letters and type of letters") do
-    expect("cat".anagram_filter).to eql("These words are anagrams")
+  context "when called on anagrams" do
+    it "should return: These words are anagrams" do
+      input_words = Anagram.new("cat", "tac")
+      expect(input_words.anagram_filter).to eql "These words are anagrams"
+    end
   end
-  it("is an antigram when no letters match") do
-    expect("cat".anagram_filter).to eql("These words are antigrams")
-  end
-  it("when any numbers found or no vowels found") do
-    expect("ytrvb123".anagram_filter).to eql("This isn't a valid entry, please enter an actual word or words!!")
-  end
-  it("spaces and punctuation removed") do
-    expect("Clint Eastwood!".anagram_filter).to eql("ClintEastwood")
-  end
-  it("word made lower case and creating an array") do
-    expect("Clint Eastwood!".anagram_filter).to eql(["c","l","i","n","t","e","a","s","t","w","o","o","d"])
-  end
-
 end
-#Clint Eastwood! = Old West action!
-#clinteastwood = oldwestaction
+
+#describe '#anagram_filter' do
+#  context "when called on antigrams" do
+#    it "should return: These words are antigrams" do
+#      input_words = Anagram.new("cat", "dog")
+#    expect(input_words.anagram_filter).to eql "These words are antigrams"
+#  end
+#end
+#
+#describe '#anagram_filter'
+#  context "when called on words that have numbers and or no vowels" do
+#    it "should return: This isn't a valid entry, please enter an actual word or words!!" do
+#      input_words = Anagram.new("grt", "h3ll0")
+#    expect(input_words.anagram_filter).to eql "This isn't a valid entry, please enter an actual word or words!!"
+#  end
+#end
+#
+#describe '#anagram_filter' do
+#  context "spaces and punctuation removed" do
+#    it "should return: ClintEastwood OldWestAction" do
+#      input_words = Anagram.new("Clint Eastwood!", "Old West Action!!")
+#    expect(input_words.anagram_filter).to eql "ClintEastwood OldWestAction")
+#  end
+#end
+#
+#describe '#anagram_filter' do
+#  context "sets letters to lowercase and creates an array" do
+#    it "should return: ["c","l","i","n","t","e","a","s","t","w","o","o","d" "o","l","d","w","e","s","t","a","c","t","i","o","n"]" ) do
+#      input_words = Anagram.new("Clint Eastwood!", "Old West Action!")
+#    expect(input_words.anagram_filter).to eql "["c","l","i","n","t","e","a","s","t","w","o","o","d" #"o","l","d","w","e","s","t","a","c","t","i","o","n"]")
+#  end
+#end
+#
+##Clint Eastwood! = Old West action!
+##clinteastwood = oldwestaction
+#
