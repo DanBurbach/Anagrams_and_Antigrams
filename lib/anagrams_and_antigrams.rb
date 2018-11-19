1338fa50df334868162b46a3b1a28f19ccd6a437
Danno <41841936+DanBurbach@users.noreply.github.com>
2018-11-19 13:31:36 -0800

    Updated rspec test working on test to pass

--- lib/anagrams_and_antigrams.rb
+++ lib/anagrams_and_antigrams.rb
@@ -1,4 +1,4 @@
-class Anagram
+class Anagram_Test
   def user_entries(word1, word2)
     @word1 = word1
     @word2 = word2
@@ -10,20 +10,19 @@ class Anagram
     @word2
   end
   def anagram_filter
-    user_entries = [@word1, @word2]
-    array_word1 = @word1.chars.to_a.sort
-    array_word2 = @word2.chars.to_a.sort
-    if user_entries.gsub(/[\d]/)&&user_entries.gsub(/[a,e,i,o,u,y]/)
+    input_words = [@word1, @word2]
+    lowercase_words = input_words.downcase!()
+    removed_symbols = lowercase_words.gsub!(/[!@#$%^&*()-=_+|;':",.<>?']/, '')
+    sifted_words = removed_symbols.gsub!(/\s+/, "")
+    if (sifted_words.gsub(/[\d]/)&&sifted_words.gsub(/[a,e,i,o,u,y]/))||(sifted_words.gsub(/[\d]/)||sifted_words.gsub(/[a,e,i,o,u,y]/)) == true
       return "This isn't a valid entry, please enter an actual word or words!!"
     else
-      user_entries.downcase!()
-      user_entries.gsub!(/[!@#$%^&*()-=_+|;':",.<>?']/, '')
-      user_entries.gsub!(/\s+/, "")
-      split_words = inputted_words.chars.to_a
+      array_word1 = @word1.chars.to_a.sort
+      array_word2 = @word2.chars.to_a.sort
       if (array_word1 == array_word2) == true
-        return "These words are anagrams"
+        return "These words are anagrams."
       else
-        return "These words are antigrams"
+        return "These words are antigrams and are not anagrams."
       end
     end
   end
--- spec/anagrams_and_antigrams_spec.rb
+++ spec/anagrams_and_antigrams_spec.rb
@@ -5,45 +5,48 @@ require('pry')
 
 describe '#anagram_filter' do
   context "when called on anagrams" do
-    it "should return: These words are anagrams" do
-      input_words = Anagram.new("cat", "tac")
-      expect(input_words.anagram_filter).to eql "These words are anagrams"
+    it "should return: These words are anagrams." do
+        input_words = Anagram_Test.new("cat", "tac")
+      expect(input_words.anagram_filter).to eql "These words are anagrams."
     end
   end
 end
-
+#
 #describe '#anagram_filter' do
 #  context "when called on antigrams" do
-#    it "should return: These words are antigrams" do
-#      input_words = Anagram.new("cat", "dog")
-#    expect(input_words.anagram_filter).to eql "These words are antigrams"
+#    it "should return: These words are antigrams and are not anagrams." do
+#      input_words = Anagram_Test.new("cat", "dog")
+#      expect(input_words.anagram_filter).to eql "These words are antigrams and are not anagrams."
+#    end
 #  end
 #end
 #
-#describe '#anagram_filter'
+#describe '#anagram_filter' do
 #  context "when called on words that have numbers and or no vowels" do
 #    it "should return: This isn't a valid entry, please enter an actual word or words!!" do
-#      input_words = Anagram.new("grt", "h3ll0")
-#    expect(input_words.anagram_filter).to eql "This isn't a valid entry, please enter an actual word or words!!"
+#      input_words = Anagram_Test.new("grt", "h3ll0")
+#      expect(input_words.anagram_filter).to eql "This isn't a valid entry, please enter an actual word or words!!"
+#    end
 #  end
 #end
 #
 #describe '#anagram_filter' do
 #  context "spaces and punctuation removed" do
 #    it "should return: ClintEastwood OldWestAction" do
-#      input_words = Anagram.new("Clint Eastwood!", "Old West Action!!")
-#    expect(input_words.anagram_filter).to eql "ClintEastwood OldWestAction")
+#      input_words = Anagram_Test.new("Clint Eastwood!", "Old West Action!!")
+#      expect(input_words.anagram_filter).to eql "ClintEastwood OldWestAction")
+#    end
 #  end
 #end
-#
+
 #describe '#anagram_filter' do
 #  context "sets letters to lowercase and creates an array" do
 #    it "should return: ["c","l","i","n","t","e","a","s","t","w","o","o","d" "o","l","d","w","e","s","t","a","c","t","i","o","n"]" ) do
-#      input_words = Anagram.new("Clint Eastwood!", "Old West Action!")
-#    expect(input_words.anagram_filter).to eql "["c","l","i","n","t","e","a","s","t","w","o","o","d" #"o","l","d","w","e","s","t","a","c","t","i","o","n"]")
+#      input_words = Anagram_Test.new("Clint Eastwood!", "Old West Action!")
+#      expect(input_words.anagram_filter).to eql "["c","l","i","n","t","e","a","s","t","w","o","o","d" #"o","l","d","w","e","s","t","a","c","t","i","o","n"]")
+#    end
 #  end
 #end
-#
-##Clint Eastwood! = Old West action!
-##clinteastwood = oldwestaction
-#
+
+#Clint Eastwood! = Old West action!
+#clinteastwood = oldwestaction
