03b461db090827d26153ee95f11ae4dec7d02c97
Danno <41841936+DanBurbach@users.noreply.github.com>
2018-11-27 21:11:22 -0800

    Have first test failing
    have first example run but with fail

--- lib/anagrams_and_antigrams.rb
+++ lib/anagrams_and_antigrams.rb
@@ -3,28 +3,23 @@ class AnagramTest
     @word1 = word1
     @word2 = word2
   end
-  def word1
-    @word1
-  end
-  def word2
-    @word2
-  end
-  def anagram_filter(words)
+
+  def anagram_filter
     words = @word1, @word2
     lowered_words = words.map(&:downcase)
-    if (lowered_words.scan(/[\d]/)&&lowered_words.scan(/[a,e,i,o,u,y]/))||(lowered_words.scan(/[\d]/)||lowered_words.scan(/[a,e,i,o,u,y]/)) == true
-      return "This isn't a valid entry, please enter an actual word or words!!"
-    end
-    punctuation = lowered_words.gsub(/[!@#$%^&*()-=_+|;':",.<>?']/, '')
-    string_words = punctuation.gsub(/\s+/, "")
-    string_words = first_word, second_word
+     if (lowered_words.count(/[\d]/)&&lowered_words.count {|c| c =~ /[a,e,i,o,u,y]/}) || (lowered_words.count(/[\d]/)||lowered_words.count.count {|c| c =~ /[a,e,i,o,u,y]/})
+       return "This isn't a valid entry, please enter an actual word or words!!"
+     end
+      punctuation1 = lowered_words[0].gsub(/[!@#$%^&*()-=_+|;':",.<>?']/, '')
+      punctuation2 = lowered_words[1].gsub(/[!@#$%^&*()-=_+|;':",.<>?']/, '')
 
-    array_word1 = @word1.chars.to_a.sort
-    array_word2 = @word2.chars.to_a.sort
-    if (array_word1 == array_word2) == true
-      return "These words are anagrams."
-    else
-      return "These words are antigrams and are not anagrams."
-    end
+      array_word1 = punctuation1.gsub(/\s+/, "")
+      array_word2 = punctuation2.gsub(/\s+/, "")
+
+      if (array_word1.chars.to_a.sort == array_word2.chars.to_a.sort)
+        return "These words are anagrams."
+      else
+        return "These words are antigrams and are not anagrams."
+      end
   end
 end
--- spec/anagrams_and_antigrams_spec.rb
+++ spec/anagrams_and_antigrams_spec.rb
@@ -11,40 +11,40 @@ describe 'AnagramTest' do
     end
   end
 
-  context "when accounting for words that may have different cases" do
-    it "returns positive regardless of being capitalized" do
-      words = AnagramTest.new("Tea", "Eat")
-      expect(words.anagram_filter()).to(eq("These words are anagrams."))
-    end
-  end
+  # context "when accounting for words that may have different cases" do
+    # it "returns positive regardless of being capitalized" do
+      # words = AnagramTest.new("Tea", "Eat")
+      # expect(words.anagram_filter()).to(eq("These words are anagrams."))
+    # end
+  # end
 
-  context "check if inputs are actual words" do
-    it "returns a negative if there are no vowels or if there are numbers in the words" do
-      words = AnagramTest.new("Cl1nt E4stw00d!", "mld Wtrt mctpln!!")
-      expect(words.anagram_filter()).to(eq("This isn't a valid entry, please enter an actual word or words!!"))
-    end
-  end
+  # context "check if inputs are actual words" do
+    # it "returns a negative if there are no vowels or if there are numbers in the words" do
+      # words = AnagramTest.new("Cl1nt E4stw00d!", "mld Wtrt mctpln!!")
+      # expect(words.anagram_filter()).to(eq("This isn't a valid entry, please enter an actual word or words!!"))
+    # end
+  # end
 
-  context "check if words are actual antigrams if they are not anagrams" do
-    it "returns a negative if no letters match" do
-      words = AnagramTest.new("hi", "bye")
-      expect(words.anagram_filter()).to(eq("These words are antigrams and are not anagrams."))
-    end
-  end
+  # context "check if words are actual antigrams if they are not anagrams" do
+    # it "returns a negative if no letters match" do
+      # words = AnagramTest.new("hi", "bye")
+      # expect(words.anagram_filter()).to(eq("These words are antigrams and are not anagrams."))
+    # end
+  # end
 
-  context "Account for multiple words being anagrams" do
-    it "returns a positive for multiple words that match up as anagrams" do
-      words = AnagramTest.new("Clint Eastwood!", "Old West Action!!")
-      expect(words.anagram_filter()).to(eq("These words are anagrams."))
-    end
-  end
+  # context "Account for multiple words being anagrams" do
+    # it "returns a positive for multiple words that match up as anagrams" do
+      # words = AnagramTest.new("Clint Eastwood!", "Old West Action!!")
+      # expect(words.anagram_filter()).to(eq("These words are anagrams."))
+    # end
+  # end
 
-  context "Account for multiple words being antigrams" do
-    it "returns a neative for multiple words that don't match up as antigrams" do
-      words = AnagramTest.new("Nintey-nine red balloons", "Old West Action!!")
-      expect(words.anagram_filter()).to(eq("These words are antigrams and are not anagrams."))
-    end
-  end
+  # context "Account for multiple words being antigrams" do
+    # it "returns a neative for multiple words that don't match up as antigrams" do
+      # words = AnagramTest.new("Nintey-nine red balloons", "Old West Action!!")
+      # expect(words.anagram_filter()).to(eq("These words are antigrams and are not anagrams."))
+    # end
+  # end
 end
 
 #Clint Eastwood! = Old West action!
